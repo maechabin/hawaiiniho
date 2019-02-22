c746a2e163c7a42a2308a534e5cc5cfb2aa32809
maechabin <mitoppo@gmail.com>
2019-02-23 01:52:24 +0900

    style対応

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 
 export default function Footer() {
+  const style = {
+    footer: {
+      padding: '80px',
+      backgroundColor: '#333',
+      color: '#fff',
+      textAlign: 'center',
+    } as React.CSSProperties,
+    copy: {
+      fontSize: '14px',
+      fontStyle: 'normal',
+    } as React.CSSProperties,
+  };
   return (
-    <footer className="footer">
-      <address className="copy">
+    <footer style={style.footer}>
+      <address style={style.copy}>
         &copy; 1997-2019
-        <a href="/">
+        <a href="/" style={{ color: '#fff' }}>
+          <img
+            style={{ verticalAlign: 'top' }}
             src="/images/nihojimamura_logo.png"
+            width="72"
+            alt="仁保島村（にほじまむら / Nihojimamura）"
+          />
         </a>
         .
       </address>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,37 @@ import * as React from 'react';
 import { withPrefix } from 'gatsby';
 
 export default function Header() {
+  const style = {
+    header: {
+      position: 'relative',
+      zIndex: 100,
+      height: '64px',
+      backgroundColor: '#0277bd',
+    } as React.CSSProperties,
+    title: {
+      margin: 0,
+      padding: 0,
+    } as React.CSSProperties,
+    title__a: {
+      lineHeight: '64px',
+      textDecoration: 'none',
+      color: '#fff',
+      fontSize: '12px',
+      fontWeight: 'normal',
+      marginLeft: '16px',
+    } as React.CSSProperties,
+  };
   return (
-    <header className="header">
-      <h1 className="title">
-        <a href="/">
+    <header style={style.header}>
+      <h1 style={style.title}>
+        <a href="/" style={style.title__a}>
           ハワイ移民資料館
+          <img
             src={withPrefix('/images/nihojimamura_logo.png')}
+            style={{ verticalAlign: 'middle' }}
+            width="120"
+            alt="仁保島村（にほじまむら）- nihojimamura"
+          />
         </a>
       </h1>
     </header>
