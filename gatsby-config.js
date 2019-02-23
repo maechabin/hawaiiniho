fef1c0f768e5017727a745580a5c500b1aa8f687
maechabin <mitoppo@gmail.com>
2019-02-24 04:07:06 +0900

    emotion導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
+  ],
 };
