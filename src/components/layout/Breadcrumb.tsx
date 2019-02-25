8ab6e4d61fa8415f19f611b99381cddcc7cd38bd
maechabin <mitoppo@gmail.com>
2019-02-25 09:13:23 +0900

    パンくず対応

---
+++ src/components/layout/Breadcrumb.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+export default function Bredcrumb(props: { title: string }): JSX.Element {
+  const { title } = props;
+  const style = {
+    breadcrumb: {
+      padding: '8px 16px',
+      margin: 0,
+      backgroundColor: '#eee',
+    },
+    next: {
+      padding: '0 8px',
+    },
+  };
+  return (
+    <p style={style.breadcrumb}>
+      <span itemScope itemType="http://data-vocabulary.org/Breadcrumb">
+        <Link to="/" itemProp="url">
+          仁保島村トップページ
+        </Link>
+      </span>
+      <span style={style.next}>></span>
+      <b itemScope itemType="http://data-vocabulary.org/Breadcrumb">
+        {title}
+      </b>
+    </p>
+  );
+}
