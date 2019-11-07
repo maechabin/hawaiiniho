8387a4c8156ad3a2269a945614833cdb0c2f38f2
maechabin <mitoppo@gmail.com>
2019-11-07 04:14:07 -1000

    特別展示資料の体裁整えた

--- src/components/collection/Main.tsx
+++ src/components/collection/Main.tsx
@@ -64,6 +64,14 @@ export default function Main(): JSX.Element {
       alt: 'ハワイ移民資料館の展示物12',
     },
   ];
+  const paragraph = {
+    color: '#333',
+    margin: '16px auto',
+    fontSize: '22px',
+    lineHeight: 1.8,
+    textAlign: 'justify',
+    marginBottom: '32px',
+  } as React.CSSProperties;
   const style = {
     main: {
       margin: '40px 0',
@@ -72,15 +80,22 @@ export default function Main(): JSX.Element {
       margin: '0',
       fontSize: '40px',
       textAlign: 'center',
+      padding: '0 32px',
     } as React.CSSProperties,
     paragraph: {
-      color: '#333',
-      margin: '16px auto',
+      ...paragraph,
+      padding: '0 32px',
       maxWidth: '880px',
-      fontSize: '22px',
-      lineHeight: 1.8,
-      textAlign: 'justify',
-      marginBottom: '32px',
+    },
+    ol: {
+      ...paragraph,
+      margin: '0 auto',
+      maxWidth: '880px',
+      fontSize: '24px',
+      border: '1px solid #424242',
+      padding: '32px 32px 16px 64px',
+      boxSizing: 'border-box',
+      borderRadius: '8px',
     } as React.CSSProperties,
     exhibit: {
       textAlign: 'center',
@@ -121,35 +136,39 @@ export default function Main(): JSX.Element {
 
       <div style={style.exhibit}>{exibit}</div>
 
-      <h2 style={style.title}>特別展示資料(平和教育資料)</h2>
+      <h2 style={style.title}>特別展示資料（平和教育資料）のご紹介</h2>
 
       <p style={style.paragraph}>
         当館では、ハワイ移民と密接に関係する太平洋戦争に関する資料を常設展示しています。
         平和教育を考える資料としても活用して戴けたら幸いです。
       </p>
 
-      <ol style={style.paragraph}>
-        <li>1941(昭和16)年 日本国内における「開戦のビラ」</li>
-        <li>1941(昭和16)年 ハワイの日本語新聞の真珠湾攻撃ニュース</li>
+      <ol style={style.ol}>
+        <li>1941年（昭和16年）日本国内における「開戦のビラ」</li>
+        <li>1941年（昭和16年）ハワイの日本語新聞の真珠湾攻撃ニュース</li>
         <li>
-          1945(昭和20)年 ハワイで印刷した伝単 (飛行機から日本の空にまかれたプロバガシダ・ビラ)
+          1945年（昭和20年）ハワイで印刷した伝単（飛行機から日本の空にまかれたプロパガンダ・ビラ）
         </li>
-        <li>1945(昭和20)年 日本国内における「終戦のビラ(詔書)」</li>
+        <li>1945年（昭和20年）日本国内における「終戦のビラ（詔書）」</li>
         <li>
-          原爆慰霊碑の碑文草案(現存する貴重な1枚)
+          <strong style={{ color: '#ff1744', fontWeight: 'normal' }}>
+            原爆慰霊碑の碑文草案（現存する貴重な1枚）
+          </strong>
           <p>
-            <img src="/images/collections/dsc_3225.jpg" width="320" />
-            <img src="/images/collections/dsc_3227.jpg" width="320" />
+            <img src="/images/collections/dsc_3225.jpg" width="320" alt="原爆慰霊碑の碑文草案" />
+            <img src="/images/collections/dsc_3227.jpg" width="320" alt="原爆慰霊碑の碑文" />
+          </p>
+          <p style={{ fontSize: '20px' }}>
+            この草案は、考案者の<strong>雑賀忠義</strong>（さいかただよし /
+            1894－1961）広島大学教授が原爆被災者の救護活動に感謝され広島医師会長宛てに贈られたものです。
+            <strong>「宛先 広島県医師会長」</strong>
+            <strong>「広島大学皆実分校内郵便局消印」</strong>
+            <strong>「当時の新聞記事の切抜き」</strong>
+            が添えられており、出典来歴が明らかです。まさしく1級の資料であることには間違いありません。
+            広島市公文書館にも似たようなものがありますが、出典来歴の物的証明もなく、公開もしていません。
           </p>
         </li>
       </ol>
-
-      <p style={style.paragraph}>
-        この草案は、考案者の雑賀忠義広島大学教授が原爆被災者の救護活動に感謝され広島医師会長宛てに贈られたものです。
-        「宛先 広島県医師会長」「広島大学皆実分校内郵便局消印」 「当時の新聞記事の切抜き」
-        が添えられており、出典来歴が明らかです。まさしく1級の資料であることには間違いありません。
-        広島市公文書館にも似たようなものがありますが、出典来歴の物的証明もなく、公開もしていません。
-      </p>
     </main>
   );
 }
