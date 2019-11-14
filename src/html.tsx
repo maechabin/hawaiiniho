d9ac1abcc5801bf6023405d0688433ee42b1f0f9
maeda <takanori@stroly.jp>
2019-11-15 02:33:08 +0900

    Resource Hints対応

--- src/html.tsx
+++ src/html.tsx
@@ -19,6 +19,8 @@ export default function HTML(props: Props): JSX.Element {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://www.google.com"></link>
+        <link rel="preconnect" href="https://maps.googleapis.com"></link>
         {props.headComponents}
         {head.title.toComponent()}
         <script src="/js/jquery.js" defer />
